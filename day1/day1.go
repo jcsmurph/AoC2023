248495796b945f3032dd4c84cbe3e9ebff0668f3
Jake Murphy <jcmurph1228@gmail.com>
2023-12-01 11:23:10 -0800

    Change day1 solution one
    Changed day1 solution one to one pass instead of two

--- day1/day1.go
+++ day1/day1.go
@@ -19,38 +19,22 @@ func Day1Solution() {
 	result := 0
 	scanner := bufio.NewScanner(input)
 
-	var numbers []int
 	for scanner.Scan() {
-		for _, char := range scanner.Text() {
-			number, err := strconv.Atoi(string(char))
-			if err != nil {
-				continue
+		leftMost := -1
+		rightMost := -1
+		for i := 0; i < len(scanner.Text()); i++ {
+			if _, err := strconv.Atoi(string(scanner.Text()[i])); err == nil {
+				if leftMost == -1 {
+                    leftMost, err = strconv.Atoi(string(scanner.Text()[i]))
+                    rightMost, err = strconv.Atoi(string(scanner.Text()[i]))
+				} else {
+                    rightMost, err = strconv.Atoi(string(scanner.Text()[i]))
+				}
 			}
-			numbers = append(numbers, number)
-			break
 		}
-
-		for i := len(scanner.Text()) - 1; i >= 0; i-- {
-			number, err := strconv.Atoi(string(scanner.Text()[i]))
-			if err != nil {
-				continue
-			}
-			numbers = append(numbers, number)
-			break
-		}
-
-        result += sliceToInt(numbers)
-        numbers = nil
+		result += (leftMost * 10) + rightMost
 
 	}
 
-    fmt.Println(result)
-}
-
-func sliceToInt(slice []int) int {
-	result := 0
-	for i := 0; i < len(slice); i++ {
-		result = result*10 + slice[i]
-	}
-	return result
+	fmt.Println(result)
 }
